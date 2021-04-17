e8ddf53b0ca5e2986bc456492caba862d5fe4540
Taha Ertuğrul ALP <alpertugrul37@gmail.com>
2021-04-17 12:40:21 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        //Bir iş sınıfı başka sınıfları new'lemez.
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            //İş kodları
+
+
+            return _carDal.GetAll();
+        }
+    }
+}
